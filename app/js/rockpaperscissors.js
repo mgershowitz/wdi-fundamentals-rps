92a914b529f1831e4fdebd94b8f4192ec25b6671
Matt Gershowitz <mattgershowitz@Matts-MBP.fios-router.home>
2016-05-14 14:56:35 -0400

    changed tie condition to else logic and cleaned up the logic statements

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -40,22 +40,21 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
-    if (playerMove == computerMove) {
-    	winner = "tie";
-    } else if (playerMove == "rock" && computerMove == "paper") {
+    if (playerMove == "rock" && computerMove == "paper") {
     	winner = "computer";
     } else if (playerMove == "rock" && computerMove == "scissors") {
-    	winner "player";
-    } else if (playerMove == "paper" && computerMove == "rock") {
     	winner = "player";
     } else if (playerMove == "paper" && computerMove == "scissors") {
     	winner = "computer";
-    } else if (playerMove == "scissors" && computerMove == "paper") {
+    } else if (playerMove == "paper" && computerMove == "rock") {
     	winner = "player";
     } else if (playerMove == "scissors" && computerMove == "rock") {
     	winner = "computer";
-    } console.log(winner);
-    return winner;
+    } else if (playerMove == "scissors" && computerMove == "paper") {
+    	winner = "player";
+    } else {
+    	winner = "tie";
+    } return winner;
 }
 
 function playToFive() {
