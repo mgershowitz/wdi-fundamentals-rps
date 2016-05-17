5b5d49a618031d0ea765b4f429618612cbb4d664
Matt Gershowitz <mattgershowitz@Matts-MBP.fios-router.home>
2016-05-16 21:31:39 -0400

    HW 3 and 4 are correct

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -18,7 +18,7 @@ function randomPlay() {
     }
 }
 ////////////////////////////////////////////////
-/*           Write Your Code Here          */
+/*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
@@ -36,6 +36,8 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
     var winner;
+    playerMove = getPlayerMove();
+    computerMove = getComputerMove();
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
